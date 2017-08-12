996891e7a000fda05efe64604878bc48c815f2c3
Brad Beltowski <bbelowski@fullsail.com>
2017-08-11 20:05:31 -0400

    Working on Redux reducer composition

--- src/Redux/Reducers/comments.js
+++ src/Redux/Reducers/comments.js
@@ -1,12 +1,26 @@
-function comments(state=[], action) {
-	switch (action.type) {
-		case 'ADD_COMMENT' :
-				console.log(action)
+function postComments(state=[], action) {
+		switch(action.type) {
+			case 'ADD_COMMENT': 
 				return state
-			default:
+			case 'REMOVE_COMMENT': 
 				return state
+			default: 
+				return state
+		}
 		return state
-	}
+}
+
+function comments(state=[], action) {
+		if(typeof action.postId !== 'undefined') {
+			// return a copy of state but tack on a new copy of the post passed in
+			// [keyOfTheObject] : function above being passed a copy of the single post
+			// and the either action object
+			return {
+				...state,
+				[action.postId] : postComments(state[action.postId], action)
+			}
+		}
+		return state
 }
 
 export default comments
