46a660526ce886815f7dcc704c25d69f8d141169
Brad Beltowski <bbelowski@fullsail.com>
2017-09-27 18:19:37 -0400

    Made main components

---
+++ src/Components/PhotoGrid.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PhotoGrid extends Component {
+	render() {
+		return <div>PhotoGrid View</div>;
+	}
+}
+
+export default PhotoGrid;
---
+++ src/Components/Single.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Single extends Component {
+	render() {
+		return <div>Single Photo View</div>;
+	}
+}
+
+export default Single;
