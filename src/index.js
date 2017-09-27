fdb8c4aefb9a09ad7461051978dd0adfc12baff7
Brad Beltowski <bbelowski@fullsail.com>
2017-09-27 18:42:51 -0400

    Setup react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './Components/App';
+import Single from './Components/Single';
+import PhotoGrid from './Components/PhotoGrid';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+	<Router>
+		<nav>
+			<Route exact path="/" render={() => (
+					<App>
+						<PhotoGrid />
+					</App>
+			)}/>
+			<Route path="/view/:postId" render={({match}) => (
+					<App>
+						<Single match={match}/>
+					</App>
+			)}/>
+		</nav>
+	</Router>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
 
 registerServiceWorker();
