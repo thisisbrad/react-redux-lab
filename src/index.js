7b2f19d8ee710b5dd996576598cefcf5aa7373c1
Brad Beltowski <bbelowski@fullsail.com>
2017-09-27 18:14:58 -0400

    Starting on Components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import App from './Components/App';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
