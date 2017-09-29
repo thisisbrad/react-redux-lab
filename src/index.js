ffe70e665f5c5c614ac565d341467eb97ffe199d
Brad Beltowski <bbelowski@fullsail.com>
2017-09-29 18:22:48 -0400

    Connected react-router-redux into the navigation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import store, { history } from './Redux/Store';
 
@@ -13,20 +14,22 @@ import PhotoGrid from './Components/PhotoGrid';
 
 //prettier-ignore
 const router = (
-	<Router>
-		<nav>
-			<Route exact path="/" render={() => (
-					<App>
-						<PhotoGrid />
-					</App>
-			)}/>
-			<Route path="/view/:postId" render={({match}) => (
-					<App>
-						<Single match={match}/>
-					</App>
-			)}/>
-		</nav>
-	</Router>
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<nav>
+				<Route exact path="/" render={() => (
+						<App>
+							<PhotoGrid />
+						</App>
+				)}/>
+				<Route path="/view/:postId" render={({match}) => (
+						<App>
+							<Single match={match}/>
+						</App>
+				)}/>
+			</nav>
+		</ConnectedRouter>
+	</Provider>
 )
 
 ReactDOM.render(router, document.getElementById('root'));
