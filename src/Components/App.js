394fcc1ba24e2afbe6c70019aa08976a12dc061f
Brad Beltowski <bbelowski@fullsail.com>
2017-09-29 20:05:09 -0400

    Connected Redux store and actions to the Main component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../Redux/Actions';
+import Main from './Main';
 
-//prettier-ignore
-class App extends Component {
-	render() {
-		return (
-			<div>
-				{/* Moved App to Main */}
-			</div>
-		);
-	}
+// Takes all the state from the Store and passes it down
+// as props to every child component
+function mapStateToProps(state) {
+	return {
+		posts: state.posts,
+		comments: state.comments
+	};
 }
+
+// Takes all action dispathers and sets them
+// as props to every child component
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators(actionCreators, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps)(Main);
 
 export default App;
