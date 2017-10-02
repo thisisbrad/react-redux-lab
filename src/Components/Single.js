ed34e7a9298da5ec6accb889bec4ddde85a3aed8
Brad Beltowski <bbelowski@fullsail.com>
2017-10-02 18:53:42 -0400

    displaying single post details

--- src/Components/Single.js
+++ src/Components/Single.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 class Single extends Component {
 	render() {
-		return <div>Single Photo View</div>;
+		// Grab the index from routes
+		const { postId } = this.props.match.params;
+		const i = this.props.posts.findIndex(post => post.code === postId);
+		// Use the index to display single post and comments
+		const post = this.props.posts[i];
+		return (
+			<div className="single-photo">
+				<Photo key={i} i={i} post={post} {...this.props} />
+			</div>
+		);
 	}
 }
 
