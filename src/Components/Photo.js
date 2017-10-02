633217c52e3d4fbaf3f9bd2b70aafaf2c481b5d0
Brad Beltowski <bbelowski@fullsail.com>
2017-10-01 21:22:42 -0400

    Displayed data on the Photo component

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -3,18 +3,15 @@ import { Link } from 'react-router-dom';
 
 class Photo extends Component {
 	render() {
-		const { post, i, comments } = this.props;
+		const { post, i, comments, increment } = this.props;
 		return (
 			<figure>
 				<Link to={`/view/${post.code}`}>
-					<img
-						src={post.display_src}
-						alt={post.caption}
-						width="200"
-						height="200"
-					/>
+					<img src={post.display_src} alt={post.caption} />
 				</Link>
 				<p>{post.caption}</p>
+				<button onClick={() => increment(i)}> &hearts; {post.likes} </button>
+				<p>{comments[post.code] ? comments[post.code].length : 0}</p>
 			</figure>
 		);
 	}
